df98a9262ea3172f4ddd23cf2ea6795aeb4bcea8
bruno teixeira <btex88@gmail.com>
2022-04-21 23:43:51 -0300

    fix: remove console log

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,7 @@ import { handleRomans, handleValues, handleResult } from '../helpers';
 
 export default function getRomanValues(text: string): [number, object] {
   const romansArr = handleRomans(text);
-  console.log('01 - ', romansArr);
   const valuesArr = handleValues(romansArr);
-  console.log('02 - ', valuesArr);
   const resultObj = handleResult(romansArr, valuesArr)
-  console.log('03 - ', resultObj);
   return [200, resultObj];
 }
