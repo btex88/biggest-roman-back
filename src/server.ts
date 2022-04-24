1c917d7e641713257fec3c13c444f49c3e9d9315
bruno teixeira <btex88@gmail.com>
2022-04-24 15:51:43 -0300

    fix: add cors lib to handle allow cors orign

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 // Default libs used throughout the project
 import { green, magenta, white } from 'colorette';
 import express, { Application } from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 // Express variable that will manage REST methods
@@ -12,6 +13,9 @@ const app: Application = express();
 // Variable to set default port that will be used
 const PORT: any = process.env.PORT ?? 8080;
 
+// Takes care of CORS middleware
+app.use(cors());
+
 // Takes care of JSON data
 app.use(express.json());
 
@@ -19,4 +23,6 @@ app.use(express.json());
 app.use('/', routes);
 
 // Server
-app.listen(PORT, () => console.log(green(`[SUCCESS] - ${white('Listening on port')} ${magenta(PORT)}`)));
+app.listen(PORT, () =>
+  console.log(green(`[SUCCESS] - ${white('Listening on port')} ${magenta(PORT)}`)),
+);
